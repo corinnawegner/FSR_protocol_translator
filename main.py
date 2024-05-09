572f0cbe39f37c03da7113f73b33ad1b19e8f67b
Corinna Wegner <corinna.wegner@outlook.de>
2024-05-09 20:08:11 +0200

    Tim's function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+DEEPL_AUTH_KEY = "a6da2cc8-76a5-40e4-8a2c-fe08b7aa288c:fx"
+
 def read_german_protocol():
     pass
 
@@ -12,4 +14,32 @@ def create_english_protocol():
 
 def write_document():
     pass
+
+def translate_agenda(agenda): #Written by Tim
+    tops = agenda.split("><li>")[1:]
+
+    for (i, top) in enumerate(tops):
+        tops[i] = top[:top.find("<")]
+
+    payload = {
+        "text" : tops,
+        "target_lang" : "EN-US"
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"DeepL-Auth-Key {DEEPL_AUTH_KEY}"
+    }
+
+    response = requests.post(
+        DEEPL_URL,
+        json = payload,
+        headers = headers
+    )
+
+    agenda_english = agenda
+
+    for (i, top) in enumerate(tops):
+        agenda_english = agenda_english.replace(top, response.json()["translations"][i]["text"])
+
+    return agenda_english
 
